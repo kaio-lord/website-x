589cda6e07518a56bab327bec336e60c9122b98a
kaio-lord <84406551+kaio-lord@users.noreply.github.com>
2024-08-06 15:23:21 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/search/',
-    bare:'https://juniorhighmaths.tafca.co.uk/bare/',
+    bare:'https://mighty-katerina-user-window-s-34d4600e.koyeb.app/bare/', // Bare server by Realm Services based on Metallic.
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: './uv/uv.handler.js',
